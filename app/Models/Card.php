c93b581ed30ccfd549b1d68c9aba0878147f9c65
EmmanuelHra601 <guerreroherrerafcoemmanuel@gmail.com>
2022-10-21 17:34:07 -0500

    Feat/SaveCard

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -7,5 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'card_name',
+        'user_id',
+        'card_number',
+        'card_expiration_date',
+        'card_cvc' ,
+        'stock',
+        'created_at',
+        'updated_at'
+    ];
 }
